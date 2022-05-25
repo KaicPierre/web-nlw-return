8da2951d1b9eaba469b72881398b0a187192b201
Kaic Pierre <kaicpierresilva2002@gmail.com>
2022-05-25 10:16:53 -0300

    stage 2 finished

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,32 @@
-export function FeedbackSuccessStep(){
-  return ()
+import { CloseButton } from "../../CloseButton";
+import successImageUrl from '../../../assets/success.svg'
+
+interface IFeedbackSuccessStepsProps {
+  onFeedbackRestartRequested: () => void 
+}
+
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: IFeedbackSuccessStepsProps){
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successImageUrl} alt="Check de sucesso" />
+      </div>
+
+      <span className="text-xl mt-2">Agradecemos o feedback!</span>
+
+      <button
+        onClick={onFeedbackRestartRequested}
+        type="button"
+        className="py-2 px-6 my-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"  
+      >
+        Quero enviar outro
+      </button>
+
+    </>
+  )
 }
